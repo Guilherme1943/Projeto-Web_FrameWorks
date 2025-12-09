f89f554e32233aed5a53f60d407c53199077b7eb
lucasemanuelcrc <lucasrodriguesrc1@gmail.com>
2025-12-08 23:44:46 -0300

    removendo authlogin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import { AutorizacaoLogin } from "@/components/autorizacao-login";
+
 
 export default function RootLayout({
   children,
@@ -12,9 +12,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
-        <AutorizacaoLogin>
+
           {children}
-        </AutorizacaoLogin>
+
         <Toaster />
       </body>
     </html>
